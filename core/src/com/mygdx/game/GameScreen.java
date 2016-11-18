21e37ab67c295921e726e62a13f7df8fa7c2f098
Chattiwa Kanarat <Chattiwa Kanarat>
2016-11-18 15:47:07 +0700

    final

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
  
 public class GameScreen extends ScreenAdapter {
